4a43688128681540b16d4327fdedccf93ffb5d1d
Michael L <mike@michaellawrenceit.com>
2024-05-28 17:34:16 -0500

    Update oval_generator.go

--- oval_generator.go
+++ oval_generator.go
@@ -7,28 +7,27 @@ import (
 	"github.com/quay/goval-parser/oval"
 )
 
-// writeOVALDefinitionsToFile writes OVAL definitions to a file
+// writeOVALDefinitionsToFile writes OVAL definitions to an XML file
 func writeOVALDefinitionsToFile(definitions *oval.Definitions, filename string) error {
-	// Generate OVAL XML
-	ovalXML, err := definitions.MarshalBytesXML()
+	xmlData, err := definitions.ToXML()
 	if err != nil {
-		return fmt.Errorf("failed to generate OVAL XML: %v", err)
+		return fmt.Errorf("error converting definitions to XML: %v", err)
 	}
 
-	// Write OVAL XML to the specified file
-	err = os.WriteFile(filename, ovalXML, 0644)
-	if err != nil {
-		return fmt.Errorf("error writing OVAL file: %v", err)
+	if err := ioutil.WriteFile(filename, xmlData, 0644); err != nil {
+		return fmt.Errorf("error writing definitions to file: %v", err)
 	}
 
-	fmt.Println("OVAL definitions written to", filename)
 	return nil
 }
 
-func main() {
-	// Assuming 'definitions' is already populated with OVAL definitions
+// GenerateOVALDefinitions generates OVAL definitions and writes them to a file
+func GenerateOVALDefinitions() {
+	definitions, err := generateOVALDefinitions(doc)
+	if err != nil {
+		log.Fatalf("Error generating OVAL definitions: %v", err)
+	}
 
-	// Write OVAL definitions to an XML file
 	if err := writeOVALDefinitionsToFile(definitions, "GLSA.oval"); err != nil {
 		log.Fatalf("Error writing OVAL definitions to file: %v", err)
 	}
